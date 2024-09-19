96e079c0565905f738e56cfe54b7998df7882920
Harsh <harsh.gupta@cognustechnology.com>
2024-09-19 17:18:01 +0530

    details added

--- lib/dashboard_module/view/mainboard_ipo_details_view.dart
+++ lib/dashboard_module/view/mainboard_ipo_details_view.dart
@@ -12,6 +12,7 @@ final _mainBoardIpoDetailsController = Get.put(MainBoardDetailsController());
 
 class MainboardIpoDetailsView extends StatefulWidget {
   const MainboardIpoDetailsView({super.key, required this.slug, required this.name});
+
   final String slug;
   final String name;
 
@@ -43,54 +44,68 @@ class _MainboardIpoDetailsViewState extends State<MainboardIpoDetailsView> {
                 Container(
                   padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
                   decoration: AppBoxDecoration.getBoxDecoration(),
-                  child: Row(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      state?.logoUrl?.contains("http") == true
-                          ? CachedImageNetworkContainer(
-                              height: 45,
-                              width: 45,
-                              decoration: AppBoxDecoration.getBoxDecoration(
-                                borderRadius: 10,
-                              ),
-                              url: state?.logoUrl,
-                              placeHolder:
-                                  buildPlaceholder(name: state?.growwShortName, context: context),
-                            )
-                          : Container(
-                              height: 45,
-                              width: 45,
-                              decoration: AppBoxDecoration.getBoxDecoration(
-                                borderRadius: 10,
-                              ),
-                              child: SvgPicture.asset(
-                                getLogoPath(state?.growwShortName ?? "-"),
-                              ),
+                      Row(
+                        children: [
+                          state?.logoUrl?.contains("http") == true
+                              ? CachedImageNetworkContainer(
+                                  height: 45,
+                                  width: 45,
+                                  decoration: AppBoxDecoration.getBoxDecoration(
+                                    borderRadius: 10,
+                                  ),
+                                  url: state?.logoUrl,
+                                  placeHolder: buildPlaceholder(
+                                      name: state?.growwShortName, context: context),
+                                )
+                              : Container(
+                                  height: 45,
+                                  width: 45,
+                                  decoration: AppBoxDecoration.getBoxDecoration(
+                                    borderRadius: 10,
+                                  ),
+                                  child: SvgPicture.asset(
+                                    getLogoPath(state?.growwShortName ?? "-"),
+                                  ),
+                                ),
+                          const SizedBox(width: 16),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  state?.companyName ?? "-",
+                                  maxLines: 2,
+                                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                                      color: AppColors.onyx, fontWeight: FontWeight.w500),
+                                ),
+                                if (state?.listing != null ||
+                                    state?.listing?.listedOn?.isNotEmpty == true) ...[
+                                  const SizedBox(height: 4),
+                                  ...List.generate(
+                                    state?.listing?.listedOn?.length ?? 0,
+                                    (index) {
+                                      return Container(
+                                        decoration: AppBoxDecoration.getBoxDecoration(
+                                          showShadow: false,
+                                          color: AppColors.porcelain,
+                                          borderRadius: 6,
+                                        ),
+                                        padding:
+                                            const EdgeInsets.symmetric(horizontal: 10, vertical: 4),
+                                        child: Text(state?.listing?.listedOn?[index] ?? "-"),
+                                      );
+                                    },
+                                  )
+                                ]
+                              ],
                             ),
-                      const SizedBox(width: 16),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              state?.companyName ?? "-",
-                              maxLines: 2,
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyLarge
-                                  ?.copyWith(color: AppColors.onyx, fontWeight: FontWeight.w500),
-                            ),
-                            if (state?.listing != null ||
-                                state?.listing?.listedOn?.isNotEmpty == true) ...[
-                              ...List.generate(
-                                state?.listing?.listedOn?.length ?? 0,
-                                (index) {
-                                  return Text(state?.listing?.listedOn?[index] ?? "-");
-                                },
-                              )
-                            ]
-                          ],
-                        ),
+                          ),
+                        ],
                       ),
+                      const SizedBox(height: 16),
                     ],
                   ),
                 ),
